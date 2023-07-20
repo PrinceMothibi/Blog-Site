5ccc67f76b08c5117e0463201857926534b6a421
Ofentse Mothibi <ofmothi022@student.wethinkcode.co.za>
2023-07-20 12:05:16 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from crypt import methods
+from datetime import datetime
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer ,primary_key=True)
+    title = db.Column(db.String(50),nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    content = db.Column(db.String(5000))
+
+    def __repr__(self):
+        return '<Blog %r>' %self.id
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        task_title = request.form['title']
+        blog_content=' '
+        blogs = Todo(title=task_title,content=blog_content)
+        try:
+            db.session.add(blogs)
+            db.session.commit()
+            id=blogs.id
+            return redirect(f'/newblog/{id}/')
+        except:
+            return 'there was an error creating your blog'
+        
+        # 'redirect to blogpost page, correlates with the Todo.id'
+    else:
+        blogs = Todo.query.order_by(Todo.date_posted).all()
+        return render_template('home.html', blogs=blogs)
+
+@app.route('/newblog/<int:id>/', methods=["GET","POST"])
+def new_blog(id):
+    # render_template('newblog.html')
+    blog=Todo.query.get_or_404(id)
+
+    if methods=='POST':
+        blog.content=request.form['blog_content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue uploading your blog'
+    else:
+        return render_template('newblog.html', blog=blog)
+
+@app.route('/viewblog/<int:id>')
+def viewblog(id):
+    return 'this is the blog'
+
+if __name__ == '__main__':
+    app.run(debug=True)
